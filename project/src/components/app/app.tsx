48e82168cfa03b5f781d3d2196d299d5b33c8740
Alex48Yakunin <39828189+Alex48Yakunin@users.noreply.github.com>
2023-03-09 11:56:22 +0300

    + добавлен роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,33 @@
 import MainPage from '../../pages/main-page/main-page';
+import Login from '../../pages/login/login';
+import { default as Room } from '../../pages/property/property';
+import NotFound from '../../pages/not-found/not-found';
+import Layout from '../../components/layout/layout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 type AppProps = {
   offersCount: number;
+};
+
+enum Location {
+  main = '/',
+  login = '/login',
+  room = '/offer/',
 }
 
-function App({offersCount}: AppProps): JSX.Element {
-  return <MainPage offersCount={offersCount}/>;
+function App({ offersCount }: AppProps): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={Location.main} element={<Layout />}>
+          <Route index element={<MainPage offersCount={offersCount} />} />
+          <Route path={Location.login} element={<Login />} />
+          <Route path={`${Location.room}:id`} element={<Room />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -5,42 +5,7 @@ type MainPageProps = {
 
 function MainPage({ offersCount }: MainPageProps): JSX.Element {
   return (
-    <>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <button className="header__logo-link header__logo-link--active">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </button>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <div className="header__nav-profile">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                  </div>
-                </li>
-                <li className="header__nav-item">
-                  <button className="header__nav-link">
-                    <span className="header__signout">Sign out</span>
-                  </button>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+    <div className="page page--gray page--main">
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -126,7 +91,7 @@ function MainPage({ offersCount }: MainPageProps): JSX.Element {
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
